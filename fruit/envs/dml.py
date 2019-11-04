bd994778a7f7e6cadd52c8fd0d4b74b6d237554b
Duy Nguyen <garlicdevs@gmail.com>
2019-11-04 23:12:12 +1100

    Add pygame for deepmind lab

--- fruit/envs/dml.py
+++ fruit/envs/dml.py
@@ -1,12 +1,14 @@
 import random
+import os
 import deepmind_lab
+import pygame
 from fruit.envs.base import BaseEnvironment
 import numpy as np
 
 
 class DeepMindLabEnvironment(BaseEnvironment):
-    def __init__(self, screen_width=300, screen_height=300, runfiles_path='', state_processor=None,
-                 level_script='tests/empty_room_test', frame_skip=1, seed=None, level_directory=''):
+    def __init__(self, screen_width=600, screen_height=480, runfiles_path='', state_processor=None,
+                 level_script='tests/empty_room_test', frame_skip=1, seed=None, level_directory='', render=False):
         self.width = screen_width
         self.height = screen_height
         self.runfiles_path = runfiles_path
@@ -17,6 +19,7 @@ class DeepMindLabEnvironment(BaseEnvironment):
         self.processor = state_processor
         self.current_state = None
         self.mode = 'RGB_INTERLEAVED'
+        self.is_render = render
 
         config = {
             'width': str(screen_width),
@@ -29,9 +32,26 @@ class DeepMindLabEnvironment(BaseEnvironment):
         self.env = deepmind_lab.Lab(level_script, [self.mode], config=config)
         self.number_of_actions = len(self.env.action_spec())
 
+        # Initialize pygame
+        if self.is_render:
+            self.__init_pygame_engine()
+
+    def get_game_name(self):
+        return 'DEEPMIND LAB'
+
+    def __init_pygame_engine(self):
+        # Center the screen
+        os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+        # Init Pygame engine
+        pygame.init()
+
+        pygame.display.set_caption(self.get_game_name())
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
     def clone(self):
         return DeepMindLabEnvironment(screen_width=self.width, screen_height=self.height, state_processor=self.processor,
-                                      runfiles_path=self.runfiles_path, level_script=self.level_script,
+                                      runfiles_path=self.runfiles_path, level_script=self.level_script, render=False,
                                       frame_skip=self.frame_skip, seed=self.seed, level_directory=self.level_directory)
 
     def get_state(self):
@@ -68,7 +88,14 @@ class DeepMindLabEnvironment(BaseEnvironment):
         return None
 
     def render(self):
-        return
+        if self.is_render:
+            flip_state = np.flip(self.current_state, axis=1)
+            rot_state = np.rot90(flip_state)
+            source = pygame.surfarray.make_surface(rot_state)
+
+            self.screen.blit(source, (0, 0))
+
+            pygame.display.flip()
 
     def get_action_space(self):
         from fruit.types.priv import Space
@@ -93,25 +120,27 @@ class DeepMindLabEnvironment(BaseEnvironment):
 if __name__ == '__main__':
     from fruit.utils.image import save_rgb_image
 
-    env = DeepMindLabEnvironment()
+    env = DeepMindLabEnvironment(render=True)
 
     num_of_actions = env.get_number_of_actions()
     action_space = env.get_action_space()
 
-    action_choice = random.randint(0, num_of_actions - 1)
-    action_amount = random.randint(action_space.get_min()[action_choice],
-                                   action_space.get_max()[action_choice])
-    action = np.zeros([num_of_actions], dtype=np.intc)
-    action[action_choice] = action_amount
+    def get_random_action():
+        action_choice = random.randint(0, num_of_actions - 1)
+        action_amount = random.randint(action_space.get_min()[action_choice],
+                                       action_space.get_max()[action_choice])
+        action = np.zeros([num_of_actions], dtype=np.intc)
+        action[action_choice] = action_amount
+        return action
 
     state = env.reset()
-    for i in range(100):
-        reward = env.step(action)
+    for i in range(1000):
+        reward = env.step(get_random_action())
         next_state = env.get_state()
         is_terminal = env.is_terminal()
+        env.render()
         state = next_state
-
-        save_rgb_image(state, './test/step_{}.jpeg'.format(i))
+        # save_rgb_image(state, './test/step_{}.jpeg'.format(i))
 
         if is_terminal:
             env.reset()
