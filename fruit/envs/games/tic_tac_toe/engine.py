90bc69eb11c7850fd76ff2504bd194505954b8b4
Duy Nguyen <garlicdevs@gmail.com>
2020-02-20 00:01:39 +1100

    Update tic tac toe engine

--- fruit/envs/games/tic_tac_toe/engine.py
+++ fruit/envs/games/tic_tac_toe/engine.py
@@ -4,67 +4,10 @@ import itertools
 
 
 class TicTacToe(BaseEngine):
-    def __init__(self):
-        self.width = 3
-        self.height = 3
-        self.current_state = [[0 for _ in range(self.height)] for _ in range(self.width)]
+    def __init__(self, size=3):
+        self.size = size
+        self.current_state = [[0 for _ in range(self.size)] for _ in range(self.size)]
         self.enemy_mode = 0  # Random mode
-        self.all_states = self.__generate_states()
-
-    def __generate_states(self):
-        cbs = [p for p in itertools.product([0, 1, 2], repeat=self.width*self.height)]
-        valid_moves = []
-        i = 0
-        for c in cbs:
-            print(i)
-            i += 1
-            if self.__is_valid(c):
-                c = np.asarray(c)
-                if c in valid_moves:
-                    continue
-
-                c_arr = np.reshape(c, [self.width, self.height])
-                c_90 = np.rot90(c_arr)
-                c_90_f = c_90.flatten()
-                if c_90_f in valid_moves:
-                    continue
-
-                c_180 = np.rot90(c_90)
-                c_180_f = c_180.flatten()
-                if c_180_f in valid_moves:
-                    continue
-
-                valid_moves.append(c_arr)
-        print(len(valid_moves))
-        return valid_moves
-
-    def __is_win(self, c):
-        def __check(c):
-            if c[0].count(1) == 3 or c[1].count(1) == 3 or c[2].count(1) == 3 \
-                or c[0].count(2) == 3 or c[1].count(2) == 3 or c[2].count(2) == 3 \
-                    or list(np.diagonal(c)).count(1) == 3 or list(np.diagonal(c)).count(2) == 3:
-                return True
-
-        if __check(c):
-            return True
-        c = c.transpose()
-        if __check(c):
-            return True
-
-        return False
-
-    def __is_valid(self, c):
-        num_1 = c.count(1)
-        num_2 = c.count(2)
-        if abs(num_1-num_2) > 1:
-            return False
-        return True
-
-    def get_game_name(self):
-        return 'TIC TAC TOE'
-
-    def clone(self):
-        return TicTacToe()
 
     def get_num_of_objectives(self):
         return 1
@@ -72,29 +15,81 @@ class TicTacToe(BaseEngine):
     def get_num_of_agents(self):
         return 1
 
+    def get_game_name(self):
+        return 'TIC TAC TOE'
+
+    def clone(self):
+        return TicTacToe(self.size)
+
+    def render(self):
+        raise NotImplemented
+
+    def get_state(self):
+        return self.__convert_state()
+
     def __convert_state(self):
-        return None
+        index = 0
+        total = 0
+        for h in range(self.size):
+            for r in range(self.size):
+                total += self.current_state[h][r] * (3 ** index)
+                index += 1
+        return total
+
+    def is_terminal(self):
+        is_full = True
+        for h in range(self.size):
+            for r in range(self.size):
+                if self.current_state[h][r] == 0:
+                    is_full = False
+
+        if is_full:
+            return True
+
+        # Search rows
+        for h in range(self.size):
+            if self.current_state[h].count(1) == self.size or self.current_state[h].count(2) == self.size:
+                return True
+
+        # Search cols
+        c = np.asarray(self.current_state)
+        c = c.transpose()
+        for h in range(self.size):
+            if c[h].count(1) == self.size or c[h].count(2) == self.size:
+                return True
+
+        # Search dig 1
+        c = np.asarray(self.current_state)
+        dig = list(np.diagonal(c))
+        if dig.count(1) == self.size or dig.count(2) == self.size:
+            return True
+
+        # Search dig 2
+        c = np.asarray(self.current_state)
+        c = np.rot90(c)
+        dig = list(np.diagonal(c))
+        if dig.count(1) == self.size or dig.count(2) == self.size:
+            return True
+
+        return False
 
     def reset(self):
-        for i in range(self.width * self.height):
-            self.states[i] = 0
+        self.current_state = [[0 for _ in range(self.size)] for _ in range(self.size)]
         return self.__convert_state()
 
     def print(self):
-        for i in range(self.height):
+        for h in range(self.size):
             st = '| '
-            for j in range(self.width):
-                k = i * self.width + j
-
-                if self.states[k] == 0:
+            for r in range(self.size):
+                if self.current_state[h][r] == 0:
                     st += '_ | '
-                elif self.states[k] == 1:
+                elif self.current_state[h][r] == 1:
                     st += 'O | '
-                elif self.states[k] == 2:
+                elif self.current_state[h][r] == 2:
                     st += 'X | '
             print(st)
 
-    def step(self, action, is_enemy=False):
+    def step(self, actions):
         if self.states[action] == 0:
 
             if is_enemy:
@@ -154,12 +149,6 @@ class TicTacToe(BaseEngine):
         else:
             return -1
 
-    def render(self):
-        raise NotImplemented
-
-    def get_state(self):
-        return self.__convert_state()
-
     def __get_reward(self, r):
         # Win
         if self.states[r[0]] == 1 and self.states[r[1]] == 1 and self.states[r[2]] == 1:
@@ -171,44 +160,15 @@ class TicTacToe(BaseEngine):
         else:
             return 0
 
-    def is_terminal(self):
-        is_full = True
-        for s in self.states:
-            if s == 0:
-                is_full = False
-
-        if is_full:
-            return True
-
-        # Search rows
-        rows = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-        for r in rows:
-            if self.__get_reward(r) != 0:
-                return True
-
-        # Search cols
-        cols = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
-        for c in cols:
-            if self.__get_reward(c) != 0:
-                return True
-
-        # Search digs
-        digs = [[0, 4, 8], [2, 4, 6]]
-        for d in digs:
-            if self.__get_reward(d) != 0:
-                return True
-
-        return False
-
     def get_state_space(self):
         from fruit.types.priv import Space
-        return Space(0, 3**len(self.states), True)
+        return Space(0, 3**(self.size*self.size), True)
 
     def get_action_space(self):
-        return range(self.width * self.height)
+        return range(self.size * self.size)
 
     def get_num_of_actions(self):
-        return self.width * self.height
+        return self.size * self.size
 
 
 if __name__ == '__main__':
@@ -218,6 +178,7 @@ if __name__ == '__main__':
     print('Reset state', state)
     num_actions = game.get_num_of_actions()
     print('Num of Actions', num_actions)
+
     for i in range(1000):
         rand_action = np.random.randint(0, num_actions)
         print('Action', rand_action)
